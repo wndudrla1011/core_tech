2bb10427dc84227c8c941db801ea5154a7579451
wndudrla1011 <wndudrla1011@naver.com>
2024-06-01 17:11:29 +0900

    Spring boot version for Component-scan

--- src/main/java/IoC2/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package IoC2;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackageClasses = CoreTechApplication.class)
-public class ApplicationConfig {
-}
