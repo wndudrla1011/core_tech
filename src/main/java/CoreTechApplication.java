4df8d7975d7ab65f234ef7bdb6ab3b6bb47ca248
wndudrla1011 <wndudrla1011@naver.com>
2024-06-01 16:14:45 +0900

    added main app

---
+++ src/main/java/CoreTechApplication.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+//@SpringBootApplication
+public class CoreTechApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        System.out.println(Arrays.toString(beanDefinitionNames));
+    }
+
+}
