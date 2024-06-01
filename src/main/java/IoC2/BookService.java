93484e2ba6676d367a604bf9890569df64d7e9b5
wndudrla1011 <wndudrla1011@naver.com>
2024-06-01 16:15:16 +0900

    edited BookService (Constructor -> setter)

--- src/main/java/IoC2/BookService.java
+++ src/main/java/IoC2/BookService.java
@@ -4,7 +4,7 @@ public class BookService {
 
     BookRepository bookRepository;
 
-    public BookService(BookRepository bookRepository) {
+    public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
